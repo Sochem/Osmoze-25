660646e9bdf7495591f1651d3499d58a7744276c
BattleGear04 <karekarayoosh@gmail.com>
2025-02-23 16:13:46 +0530

    Added login and signup pages

--- osmoze/src/app/page.jsx
+++ osmoze/src/app/page.jsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="lg:flex lg:flex-1">
+      <a href="/login"
+        className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >Login
+      </a>
+    </div>
+  )
 }
