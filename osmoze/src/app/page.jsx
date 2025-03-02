c6ff6adad041723be9e3706bd5e0e63dd9feb73c
nanda2708 <nandagopal2708@gmail.com>
2025-03-02 18:54:30 +0530

    made the navbar responsive

--- osmoze/src/app/page.jsx
+++ osmoze/src/app/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { auth } from "@/firebase/firebaseConfig";
+// import { auth } from "@/firebase/firebaseConfig";
 import { signOut } from "firebase/auth";
 import Link from "next/link";
 import Footer from "./components/Footer";
